6eaade3d56c6d16899ca126ea609ed06bac157ca
Glauter Carneiro <glauterius@gmail.com>
2021-05-04 19:28:43 -0300

    Projeto criado Spring + React

---
+++ backend/src/test/java/com/glauter/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glauter.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
